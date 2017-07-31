6b639243468b0f534d8d74dcf1141e14c6d33bc7
Lena Yu <lena@Lenas-MacBook-Pro.local>
2017-07-31 13:19:26 -0400

    typing

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
 
-console.log('RxJS Boiler Running...');
+let userInput=Rx.Observable.fromEvent($('#userInput'), 'keyup');
+
+userInput.do(()=>$('#notice').text('typing...'))
+.debounceTime(1000).map(e=>e.target.value).subscribe(v=>$('#notice').text(v));
+
